27e426599c35ec6f1f48c3e89b530aeef1a0fee6
willxj36 <willxj36@gmail.com>
2021-01-09 19:43:34 -0700

    starting on inheritance section

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//using object literals
 let person1 = {
     name: 'Amber',
     sayHello: () => console.log('Hi, my name is Amber!')
@@ -28,3 +29,57 @@ person2.sayHello();
 person3.sayHello();
 person4.sayHello();
 person5.sayHello();
+
+//using old pseudoclasses (es5)
+function Person (name, age, city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+}
+
+Person.prototype.sayHello = function () {
+    console.log(`Hi, my name is ${this.name}, I'm ${this.age} years old, and I live in ${this.city}`);
+}
+
+let p1 = new Person('Amber', 25, 'Colorado Springs');
+let p2 = new Person('Ethan', 29, 'Colorado Springs');
+let p3 = new Person('Kendra', 29, 'Colorado Springs');
+let p4 = new Person('Adam', 34, 'Colorado Springs');
+let p5 = new Person('Rudy', 31, 'Colorado Springs');
+
+p1.sayHello();
+p2.sayHello();
+p3.sayHello();
+p4.sayHello();
+p5.sayHello();
+
+//using es6 classes (just made it plural so it wouldn't error due to previous function declaration)
+class Persons {
+    constructor(name, age, city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    sayHello = () => console.log(`Hi, my name is ${this.name}, I'm ${this.age} years old, and I live in ${this.city}`);
+}
+
+p1 = new Persons('Amber', 25, 'Colorado Springs');
+p2 = new Persons('Ethan', 29, 'Colorado Springs');
+p3 = new Persons('Kendra', 29, 'Colorado Springs');
+p4 = new Persons('Adam', 34, 'Colorado Springs');
+p5 = new Persons('Rudy', 31, 'Colorado Springs');
+
+p1.sayHello();
+p2.sayHello();
+p3.sayHello();
+p4.sayHello();
+p5.sayHello();
+
+//inheritance section
+class Vehicle {
+    constructor(mfr, wheels) {
+        this.mfr = mfr;
+        this.wheels = wheels;
+    }
+}
