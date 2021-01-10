4c35c31fbdc939ebb41aac98088abd4863b95d3b
willxj36 <willxj36@gmail.com>
2021-01-09 19:12:27 -0700

    copied instructions into readme, started first part of js, made basic html

---
+++ app.js
@@ -0,0 +1,30 @@
+let person1 = {
+    name: 'Amber',
+    sayHello: () => console.log('Hi, my name is Amber!')
+};
+
+let person2 = {
+    name: 'Ethan',
+    sayHello: () => console.log('Hi, my name is Ethan!')
+};
+
+let person3 = {
+    name: 'Kendra',
+    sayHello: () => console.log('Hi, my name is Kendra!')
+};
+
+let person4 = {
+    name: 'Adam',
+    sayHello: () => console.log('Hi, my name is Adam!')
+};
+
+let person5 = {
+    name: 'Rudy',
+    sayHello: () => console.log('Hi, my name is Rudy!')
+};
+
+person1.sayHello();
+person2.sayHello();
+person3.sayHello();
+person4.sayHello();
+person5.sayHello();
